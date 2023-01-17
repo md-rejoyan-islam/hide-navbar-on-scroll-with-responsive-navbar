2c8c09ba10d32af5b6a956bc90f53dac21d32a69
RejoyanIslam <rejoyanislam0014@gmail.com>
2023-01-17 22:55:15 +0600

    upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,29 +1,28 @@
-const menu=document.getElementById('menu')
-const menuBtn=document.getElementById('menuBtn')
-const header=document.getElementById('header')
+const menu = document.getElementById("menu");
+const menuBtn = document.getElementById("menuBtn");
+const header = document.getElementById("header");
 
 // menu toggle
-let result=false
-menuBtn.onclick=()=>{
-    if(!result){
-        menu.classList.remove("hidden");
-        result=true;
-    }else{
-        menu.classList.add("hidden");
-        result=false
-    }
-    
-}
+let result = false;
+menuBtn.onclick = () => {
+  if (!result) {
+    menu.classList.remove("hidden");
+    result = true;
+  } else {
+    menu.classList.add("hidden");
+    result = false;
+  }
+};
 
 // hide on scroll
-let beforeScroll=0
-window.onscroll=()=>{
-    let newScroll=window.scrollY
-    if(newScroll<beforeScroll){
-        header.classList.add('sticky','top-0')
-        beforeScroll = newScroll;
-    }else{
-        header.classList.remove("sticky", "top-0");
-        beforeScroll = newScroll;
-    }
-}
+let beforeScroll = 0;
+window.onscroll = () => {
+  let newScroll = window.scrollY;
+  if (newScroll < beforeScroll) {
+    header.classList.add("sticky", "top-0");
+    beforeScroll = newScroll;
+  } else {
+    header.classList.remove("sticky", "top-0");
+    beforeScroll = newScroll;
+  }
+};
